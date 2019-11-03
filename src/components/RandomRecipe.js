ab8c0566114edad85061ac27356eacbb9f64280b
singhaishwarya273 <singhaishwarya273@gmail.com>
2019-11-03 20:59:12 +0530

    changed

--- src/components/RandomRecipe.js
+++ src/components/RandomRecipe.js
@@ -1,112 +1,97 @@
-import React from "react";
+import React from 'react';
 
-import style from "./RandomRecipe.module.css";
+import  './RandomRecipe.css';
+
+
 
 import { connect } from "react-redux";
-import { GetDetail, SetDetail } from "../Actions";
+import {GetDetail, SetDetail} from '../Actions';
 
-const RandomRecipe = props => {
-  console.log(props.recipes);
 
-  const ing = () => {
-    let indegrients = [];
-    for (let i = 1; i < 21; i++) {
-      if (props.recipe[`strIngredient${i}`]) {
-        indegrients.push(
-          `${props.recipe[`strIngredient${i}`]} - ${
-            props.recipe[`strMeasure${i}`]
-          }`
-        );
-      }
-    }
-    //console.log(indegrients)
+const RandomRecipe= (props) =>
+{
+	console.log(props.recipes);
+	
+	const ing = ()=>{
+	let indegrients= [];
+	for (let i=1;i<21; i++){
+		if(props.recipe[`strIngredient${i}`]){
+			indegrients.push(`${props.recipe[`strIngredient${i}`]} - ${props.recipe[`strMeasure${i}`]}`);
+		}			
+		}
+		//console.log(indegrients)
+		
+		return indegrients.map(ingredient => <li>{ingredient}</li>);
+	}
+	
+	return(
+	<>
+	
+	
+	<div className="row" >
+	{props.recipe ?(<div className = "col-6" >
+	
+	
+			
+	
+				<img src={props.recipe['strMealThumb']} alt="Meal Image"/>
+				
+				{props.recipe['strCategory']
+						? (<p><strong>Category:</strong> {props.recipe['strCategory']}</p>)
+				:( '')}
+				
+				{props.recipe['strArea'] ? <p><strong>Area:</strong> {props.recipe['strArea']}</p> : ''}
+				
+				{props.recipe['strTags']
+						? <p><strong>Tags:</strong> {props.recipe['strTags'].split(',').join(', ')}</p>
+						: ''
+				}
+				<div className= "ingredient" >
+				<h4>Ingredients:</h4>
+				
+					{ing()}
+				
+				
+			</div>
+			
+			<div className="Meal">
+				<h4>{props.recipe.strMeal}</h4>
+				<div className="instruction" style= {{textAlign:'Left', padding:'10px'}}>
+				<p>{props.recipe.strInstructions}</p>
+				</div>
+			</div>
+			<hr></hr>
+		
+	
+		{props.recipe.strYoutube? (<div  style={{alignItem:"center"}}>
+			<h4 style= {{paddingBottom:'10px'}}>Video Recipe</h4>
+			
+				<iframe width="420" height="315" src={`https://www.youtube.com/embed/${props.recipe.strYoutube.slice(-11)}`}
+			frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+			picture-in-picture" allowfullscreen> </iframe>
+			</div>)
+		:("")}
+		
+		</div>): ("")}
+	
+</div>
 
-    return indegrients.map(ingredient => <li>{ingredient}</li>);
-  };
-
-  return (
-    <>
-      <div className={style.recipe}>
-        {props.recipe ? (
-          <div className={style.recipies}>
-            <div className={style.row}>
-              <img src={props.recipe["strMealThumb"]} alt="Meal" />
-
-              {props.recipe["strCategory"] ? (
-                <p>
-                  <strong>Category:</strong> {props.recipe["strCategory"]}
-                </p>
-              ) : (
-                ""
-              )}
-
-              {props.recipe["strArea"] ? (
-                <p>
-                  <strong>Area:</strong> {props.recipe["strArea"]}
-                </p>
-              ) : (
-                ""
-              )}
-
-              {props.recipe["strTags"] ? (
-                <p>
-                  <strong>Tags:</strong>{" "}
-                  {props.recipe["strTags"].split(",").join(", ")}
-                </p>
-              ) : (
-                ""
-              )}
-              <div className="ingredient">
-                <h4>Ingredients:</h4>
-
-                {ing()}
-              </div>
-
-              <div className={style.Meal}>
-                <h4>{props.recipe.strMeal}</h4>
-                <p>{props.recipe.strInstructions}</p>
-              </div>
-              <hr></hr>
-            </div>
-            {props.recipe.strYoutube ? (
-              <div className="row">
-                <h4 style={{ paddingBottom: "10px" }}>Video Recipe</h4>
-                <div className="videoWrapper">
-                  <iframe
-                    title="Recepie Video"
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${props.recipe.strYoutube.slice(
-                      -11
-                    )}`}
-                    frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; 
-			picture-in-picture"
-                    allowfullscreen
-                  >
-                    {" "}
-                  </iframe>
-                </div>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    </>
-  );
+	</>)	;
+	
+	
+	
 };
-const mapStateToProps = state => {
-  console.log(state);
-  return {
-    food: state.food
-  };
-};
+const mapStateToProps =(state)=>  {
+	 console.log(state);
+ return{
+	food: state.food
+ };
+ 
+ };
 
-export default connect(
-  mapStateToProps,
-  { GetDetail, SetDetail }
-)(RandomRecipe);
+ export default connect(mapStateToProps,{GetDetail,SetDetail})(RandomRecipe);
+
+
+
+
+
